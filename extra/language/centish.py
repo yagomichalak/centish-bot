fc6702c2378a9f21098552cce81bbb791b682263
= <=>
2021-08-20 19:22:50 -0300

    FINISHED current list of vocabulary

--- extra/language/centish.py
+++ extra/language/centish.py
@@ -5,12 +5,13 @@ class Centish:
     """ Class for the Centish language. """
 
     words_path: str = './extra/language/words.json'
-
+    
+    # {"word": "", "translation": "", "types": [], "note": "", "examples": []}
 
     @classmethod
     async def get_words(cls) -> Dict[str, Dict[str, str]]:
         """ Gets all words from the vocabulary list. """
-        
+
         data = {}
         with open(str(cls.words_path), 'r', encoding="utf-8") as f:
             data = json.loads(f.read())
