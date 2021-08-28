b6dbd3234f9a037f8dbf2bca3eac866d70266254
= <=>
2021-08-28 20:45:50 -0300

    MADE error handler

--- main.py
+++ main.py
@@ -15,6 +15,35 @@ async def on_ready() -> None:
     change_bot_status.start()
     print('Bot is ready!')
 
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You can't do that!")
+
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Please, inform all parameters!')
+
+    elif isinstance(error, commands.NotOwner):
+        await ctx.send("You're not the bot's owner!")
+
+    elif isinstance(error, commands.CommandOnCooldown):
+        await ctx.send(error)
+
+    elif isinstance(error, commands.MissingAnyRole):
+        role_names = [f"**{str(discord.utils.get(ctx.guild.roles, id=role_id))}**" for role_id in error.missing_roles]
+        await ctx.send(f"You are missing at least one of the required roles: {', '.join(role_names)}")
+
+    elif isinstance(error, commands.errors.RoleNotFound):
+        await ctx.send(f"**{error}**")
+
+    elif isinstance(error, commands.ChannelNotFound):
+        await ctx.send("**Channel not found!**")
+
+    print('='*10)
+    print(f"ERROR: {error} | Class: {error.__class__} | Cause: {error.__cause__}")
+    print('='*10)
+
+
 @tasks.loop(seconds=30)
 async def change_bot_status():
 
