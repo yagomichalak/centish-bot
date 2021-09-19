963275e5938e05519830e0cc78fb6dc2f4a1f3ed
yagomichalak <yagomichalak@gmail.com>
2021-09-19 11:13:53 -0300

    ADDED a kiss user command

--- main.py
+++ main.py
@@ -69,27 +69,64 @@ async def change_bot_status():
             type=discord.ActivityType.watching, 
             name=f"{random_word['word']} | {random_word['translation']}"))
 
-@client.command()
-async def test(ctx) -> None:
-    await ctx.reply("**Command successfully tested!**")
-
-
-@client.slash_command(guild_ids=guild_ids)  # create a slash command for the supplied guilds
-async def hello(ctx):
-    """Say hello to the bot"""  # the command description can be supplied as the docstring
-    await ctx.send(f"Hello {ctx.author}!")
-
 for filename in os.listdir('./cogs/'):
     if filename.endswith('.py'):
         client.load_extension(f"cogs.{filename[:-3]}")
 
-
-
-@client.user_command(name="pat", guild_ids=guild_ids)
+@client.user_command(name="Pat", guild_ids=guild_ids)
 async def _pat(ctx, user: discord.Member) -> None:
     """ Pats someone. """
 
     await ctx.send(f"**{ctx.author.mention} patted {user.mention}!**")
+
+@client.user_command(name="Kiss", guild_ids=guild_ids)
+async def _kiss(ctx, user: discord.Member) -> None:
+    """ Kisses someone. """
+
+    author = ctx.author
+    if user == author:
+        return await ctx.respond(f"**You cannot kiss yourself, lol!**", ephemeral=True)
+
+    if user.bot:
+        return await ctx.respond("**You cannot kiss a bot!**", ephemeral=True)
+
+    embed = discord.Embed(
+        title="__Kiss__",
+        description=f"💓 **{ctx.author.mention} kissed {user.mention}!** 💓",
+        color=discord.Color.brand_red()
+    )
+
+    kisses: List[str] = [
+            'https://c.tenor.com/sx0mJIjy61gAAAAC/milk-and-mocha-bear-couple.gif',
+            'https://c.tenor.com/hK8IUmweJWAAAAAC/kiss-me-%D0%BB%D1%8E%D0%B1%D0%BB%D1%8E.gif',
+            'https://c.tenor.com/Aaxuq2evHe8AAAAC/kiss-cute.gif',
+            'https://c.tenor.com/A0ixOj7Y1WUAAAAC/kiss-anime.gif',
+            'https://c.tenor.com/ErAPuiWY46QAAAAC/kiss-anime.gif',
+            'https://c.tenor.com/4GdkzkTQdI8AAAAC/kissing-love.gif',
+            'https://c.tenor.com/lsOkG57_ibIAAAAd/kiss-lip-kiss.gif',
+            'https://c.tenor.com/uF0-jdZRkLYAAAAC/kissing-kiss.gif',
+            'https://c.tenor.com/3kvvlXjRse0AAAAC/kiss-lip-kiss.gif',
+            'https://c.tenor.com/RZEZVjS3MeMAAAAC/forcibly-kissing-deep-kiss.gif',
+            'https://c.tenor.com/teTesMTK_c4AAAAC/shiksha-wedding.gif',
+            'https://c.tenor.com/RrUKz9kC3bUAAAAi/dovey-bunnies-kisses.gif',
+            'https://c.tenor.com/FDcq03ZLNX0AAAAi/love-couple.gif',
+            'https://c.tenor.com/LbOTuZXj9y4AAAAC/cat-love.gif',
+            'https://c.tenor.com/OhTr6EO7u14AAAAC/love-you-kiss.gif',
+            'https://c.tenor.com/xTSmPl72UjYAAAAC/kiss-blushing.gif',
+            'https://c.tenor.com/Gr8BaRWOudkAAAAC/brown-and-cony-kiss.gif',
+            'https://c.tenor.com/uhB9b3AzxrQAAAAC/brown.gif',
+            'https://c.tenor.com/cK2c6eQ7Is8AAAAC/cony-and-brown-hug.gif',
+            'https://c.tenor.com/sXNJMF5vI8oAAAAC/love-peachcat.gif',
+            'https://c.tenor.com/MXd8Xby7jnMAAAAC/davydoff-love.gif',
+            'https://c.tenor.com/yWGhrAd0cioAAAAC/kissing-couple.gif'
+
+        ]
+
+    embed.set_author(name=author, icon_url=author.display_avatar)
+    embed.set_image(url=choice(kisses))
+    embed.set_footer(text=user, icon_url=user.display_avatar)
+
+    await ctx.send(embed=embed)
 
 # Slash commands
 @client.slash_command(name="words", guild_ids=guild_ids)
