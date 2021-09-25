b49079dbbe1b97f8aafad0b15724e9d602fa81ed
yagomichalak <yagomichalak@gmail.com>
2021-09-25 11:07:56 -0300

    MADE a seduce slash command

--- cogs/roleplay.py
+++ cogs/roleplay.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from discord.app.commands import user_command, message_command, slash_command
+from discord.app.commands import Option, user_command, message_command, slash_command
 
 from typing import List
 import os
@@ -40,7 +40,7 @@ class RolePlay(commands.Cog):
 
         embed = discord.Embed(
             title="__Kiss__",
-            description=f"💓 **{ctx.author.mention} kissed {user.mention}!** 💓",
+            description=f"💓 **{author.mention} kissed {user.mention}!** 💓",
             color=discord.Color.brand_red()
         )
 
@@ -72,6 +72,69 @@ class RolePlay(commands.Cog):
 
         embed.set_author(name=author, icon_url=author.display_avatar)
         embed.set_image(url=choice(kisses))
+        embed.set_footer(text=user, icon_url=user.display_avatar)
+
+        await ctx.respond(embed=embed)
+
+    @slash_command(name="seduce", guild_ids=guild_ids)
+    async def _seduce(self, ctx, user: Option(discord.Member, name="user", description="The member to seduce.", required=True)) -> None:
+        """ Seduces someone.
+        :param user: The member to seduce. """
+
+        author = ctx.author
+        if user == author:
+            return await ctx.respond(f"**You cannot seduce yourself, lol!**", ephemeral=True)
+
+        if user.bot:
+            return await ctx.respond("**You cannot seduce a bot!**", ephemeral=True)
+
+        embed = discord.Embed(
+            title="__Seduce__",
+            description=f"😏 **{author.mention} seduced {user.mention}!** 😏",
+            color=discord.Color.dark_gold()
+        )
+
+        seduces: List[str] = [
+            'https://c.tenor.com/zM1LLF0voFIAAAAS/butt-touch.gif',
+            'https://c.tenor.com/4M8bBOPMpJcAAAAS/unzip-pants.gif',
+            'https://c.tenor.com/OgmIkGPBUj4AAAAS/daffy-duck-sexy.gif',
+            'https://c.tenor.com/HH9Mte12iuoAAAAS/girlfriends-love.gif',
+            'https://c.tenor.com/5K9wKk80s50AAAAS/good-girl-seductive.gif',
+            'https://c.tenor.com/2e8Hxp4OczYAAAAS/sexy-seductive.gif',
+            'https://c.tenor.com/4kG1mXhlIiQAAAAC/crawl-seductive.gif',
+            'https://c.tenor.com/loaJcK1FPgIAAAAS/naked-naughty.gif',
+            'https://c.tenor.com/7e9l_2L2RxsAAAAS/natalie-portman-stripping.gif',
+            'https://c.tenor.com/n7iFjPuJkYEAAAAS/flaunt-leg.gif',
+            'https://c.tenor.com/zEm4_uLqQy8AAAAS/elena-stefan-kiss.gif',
+            'https://c.tenor.com/W_dThAW2A4kAAAAS/seductive-seduce.gif',
+            'https://c.tenor.com/EkymFviqIT0AAAAC/seductive-making-out.gif',
+            'https://c.tenor.com/Fkvp0o1zWMEAAAAC/seductive-himym.gif',
+            'https://c.tenor.com/-g0xtwX3t78AAAAC/booty-sexy.gif',
+            'https://c.tenor.com/JkElZ-JKqnMAAAAC/flirt-seduce.gif',
+            'https://c.tenor.com/NrdehE76Pk4AAAAd/lets-have-sex-seducing.gif',
+            'https://c.tenor.com/ksvAUP19DqoAAAAC/seductive-wrap-around.gif',
+            'https://c.tenor.com/hYV9SOsmXwYAAAAC/seduce-sexy.gif',
+            'https://c.tenor.com/Qag8ifMBgXcAAAAC/garfield-dancing.gif',
+            'https://c.tenor.com/gJjz-12AUhAAAAAC/seduce-seduce-you.gif',
+            'https://c.tenor.com/WpLww06Z-5UAAAAC/bedroom-eyes.gif',
+            'https://c.tenor.com/kTnwtDrGa3UAAAAC/come-here-seductive.gif',
+            'https://c.tenor.com/O4dQFZIXhEsAAAAC/flirting.gif',
+            'https://c.tenor.com/DdrlDmZdz4QAAAAC/sex-take-your-shirt-off.gif',
+            'https://c.tenor.com/l7k9gBs8ZwUAAAAC/ohreally-sexy.gif',
+            'https://c.tenor.com/9tVeJFB8HIgAAAAC/seductive-sexy.gif',
+            'https://c.tenor.com/hk09Npo8YpEAAAAC/marshall-jason-segel.gif',
+            'https://c.tenor.com/9gGVdXCua4IAAAAC/zoe-hart-flirt.gif',
+            'https://c.tenor.com/ct1MXPbyJ1MAAAAd/such-beaty-smell.gif',
+            'https://c.tenor.com/Z1wp97tfbUsAAAAC/bath-legs.gif',
+            'https://c.tenor.com/ieVgdmsFlhgAAAAC/seduce-hot.gif',
+            'https://c.tenor.com/QtHsQnlce9sAAAAC/kiss-wink.gif',
+            'https://c.tenor.com/7KEZ3UywoyIAAAAC/eyebrows-seductive.gif',
+            'https://c.tenor.com/xhHErbzZz6sAAAAC/sexy-wink.gif',
+            'https://c.tenor.com/La3-ZgjSRxgAAAAC/wiggle-shimmy.gif'
+        ]
+
+        embed.set_author(name=author, icon_url=author.display_avatar)
+        embed.set_image(url=choice(seduces))
         embed.set_footer(text=user, icon_url=user.display_avatar)
 
         await ctx.respond(embed=embed)
