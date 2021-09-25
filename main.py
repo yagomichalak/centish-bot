897e3e4a45ed689d55f7a2b7ecb3105d00c1bc66
yagomichalak <yagomichalak@gmail.com>
2021-09-25 17:26:11 -0300

    MADE a help command

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import discord
-from discord.app.commands import Option
+from discord.utils import escape_mentions
 from discord.ext import commands, tasks
+
+import os
 from dotenv import load_dotenv
 load_dotenv()
-import os
-from extra.language.centish import Centish
-from extra.menus import WordPaginationView
-from extra import utils
 from random import choice
-from typing import List, Dict, Union
+from typing import List
+from extra.language.centish import Centish
 import re
 
 guild_ids: List[int] = [int(os.getenv('SERVER_ID'))]
-client = commands.Bot(command_prefix='c!', intents=discord.Intents.all())
+client = commands.Bot(command_prefix='c!', intents=discord.Intents.all(), help_command=None)
 
 @client.event
 async def on_ready() -> None:
@@ -82,6 +81,68 @@ async def change_bot_status():
         activity=discord.Activity(
             type=discord.ActivityType.watching, 
             name=f"{random_word['word']} | {random_word['translation']}"))
+
+
+@client.command()
+async def help(ctx, *, cmd: str =  None):
+    """ Shows some information about commands and categories. 
+    :param cmd: The command/category. """
+
+
+    if not cmd:
+        embed = discord.Embed(
+            title="All commands and categories",
+            description=f"```ini\nUse {client.command_prefix}help command or {client.command_prefix}help category to know more about a specific command or category\n\n[Examples]\n[1] Category: {client.command_prefix}help Language\n[2] Command : {client.command_prefix}help word_count```",
+            timestamp=ctx.message.created_at,
+            color=ctx.author.color
+            )
+
+        for cog in client.cogs:
+            cog = client.get_cog(cog)
+            cog_commands = [c for c in cog.__cog_commands__ if hasattr(c, 'parent') and c.parent is None]
+            commands = [f"{client.command_prefix}{c.name}" for c in cog_commands if not c.hidden]
+            if commands:
+                embed.add_field(
+                    name=f"• __{cog.qualified_name}:__",
+                    value=f"> {', '.join(commands)}",
+                    inline=False
+                    )
+
+        cmds = []
+        for y in client.walk_commands():
+            if not y.cog_name and not y.hidden:
+                cmds.append(f"{client.command_prefix}{y.name}")
+
+        embed.add_field(
+            name='• __Uncategorized Commands:__',
+            value=f"> {', '.join(cmds)}",
+            inline=False)
+        await ctx.send(embed=embed)
+
+    else:  
+        cmd = escape_mentions(cmd)
+        if command := client.get_command(cmd.lower()):
+            command_embed = discord.Embed(title=f"→ __Command:__ {client.command_prefix}{command.qualified_name}", description=f"__**Description:**__\n```{command.help}```", color=ctx.author.color, timestamp=ctx.message.created_at)
+            return await ctx.send(embed=command_embed)
+
+        # Checks if it's a cog
+        for cog in client.cogs:
+            if str(cog).lower() == str(cmd).lower():
+                cog = client.get_cog(cog)
+                cog_embed = discord.Embed(title=f"• __Cog:__ {cog.qualified_name}", description=f"__**Description:**__\n```{cog.description}```", color=ctx.author.color, timestamp=ctx.message.created_at)
+                cog_commands = [c for c in cog.__cog_commands__ if hasattr(c, 'parent') and c.parent is None]
+                commands = [f"{client.command_prefix}{c.name}" for c in cog_commands if not c.hidden]
+                if commands:
+                    cog_embed.add_field(
+                        name=f"→ __Commands:__",
+                        value=f"> {', '.join(commands)}",
+                        inline=False
+                        )
+                return await ctx.send(embed=cog_embed)
+        # Otherwise, it's an invalid parameter (Not found)
+        else:
+            await ctx.send(f"**Invalid parameter! It is neither a command nor a cog!**")
+
 
 for filename in os.listdir('./cogs/'):
     if filename.endswith('.py'):
